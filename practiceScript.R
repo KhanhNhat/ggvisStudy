ce82de73e2d0e6c9adb6445a7f13ae5d8e5e6bbc
KhanhNhat <hoangphamkhanhnhat@gmail.com>
2017-12-30 12:00:48 +1100

    Use interaction() to make a specific combination parameters when group_by() has more than one. Create first interactive plot with ggvis.

--- practiceScript.R
+++ practiceScript.R
@@ -14,3 +14,15 @@ mtcars %>%
 mtcars %>%
   ggvis(x =~ wt, y =~ mpg, fill =~ factor(cyl), shape := 'cross') %>%
   layer_points()
+
+mtcars %>% 
+  group_by(cyl, am) %>% 
+  ggvis(~hp, ~mpg, fill = ~interaction(cyl, am)) %>% 
+  layer_points() %>%
+  layer_model_predictions(model = "lm")
+
+mtcars %>%
+  ggvis(~hp, ~mpg,
+        shape := input_select(label = 'Choose shape:',
+                              choice = c('circle', 'square', 'diamond'))) %>%
+  layer_points()
