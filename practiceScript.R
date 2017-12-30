00f57f3be74f30d11cb32b820854d0049f11c93f
KhanhNhat <hoangphamkhanhnhat@gmail.com>
2017-12-30 19:16:17 +1100

    How to add more than one legend.

--- practiceScript.R
+++ practiceScript.R
@@ -26,3 +26,19 @@ mtcars %>%
         shape := input_select(label = 'Choose shape:',
                               choice = c('circle', 'square', 'diamond'))) %>%
   layer_points()
+
+mtcars %>%
+  ggvis(~mpg) %>%
+  layer_histograms(width = input_numeric(label = 'Choose a binwidth:',
+                                         value = 1))
+
+mtcars %>%
+  ggvis(~mpg) %>%
+  layer_histograms(width = input_slider(label = 'Choose a binwidth:',
+                                        min = 1, max = 20))
+
+mtcars %>% ggvis(x = ~wt, y = ~mpg, fill = ~factor(cyl), size = ~hp) %>%
+  layer_points() %>%
+  add_legend('fill', title = 'Num of cylinder', orient = 'right') %>%
+  add_legend('size', title = 'Horse power',
+             properties = legend_props(legend = list(y = 80)))
